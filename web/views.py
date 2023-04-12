541092b22af6ec1b336950043c04bcea8dbed5d9
Yana <yana.ilina.1999@list.ru>
2023-04-12 11:20:18 +0300

    +pages

--- web/views.py
+++ web/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+
+# создаем view, который принимает request и
+# выводит некоторый текст на экран с помощью HttpResponse
+# и передаем функцию в urls.py
+# и добавляем подключение адресов в urls.py проекта
+
+# теперь можем поменять на функцию render,
+# которая первым аргументом принимает request,
+# a 2 'web/main/html' и на страничку приходит html код
+# передаем год в шаблон ( данные в шаблоне - контекст),
+# и ключи становятся доступны для вывода
+
+def main_view(request):
+    year = datetime.now().year
+    return render(request, 'web/main/html', {
+        "year": year
+    })
